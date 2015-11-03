0337a3c108bb5f2c783011ffd60e37f5fd72b007
gerald-fongwe <geraldjam2004@yahoo.com>
2015-11-03 13:51:23 -0700

    added becon ipsuml to php

--- index.php
+++ index.php
@@ -2,34 +2,55 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8"/>
-		<link rel="stylesheet" href="javascript/style.js" type="text/javascript"/>
+<!--		<link rel="stylesheet" href="javascript/style.js" type="text/javascript"/>-->
+		<script type="text/javascript" src="javascript/style.js"></script>
 		<title>javascript exercise</title>
 	</head>
-		<body>
-	 			<header>
-					<a class="logo" href="#">Bacon Ipsum</a>
-						<div class="row">
-							<div class="col-md-12">
-								<div id="mouse" onmouseover="mouseOver();" onmouseout="mouseOut();">
-					<p>Bacon ipsum dolor amet filet mignon pancetta corned beef swine drumstick. Alcatra pancetta cow rump, bacon tail
-			biltong pork belly pork sirloin strip steak chuck. Brisket turkey swine spare ribs short ribs leberkas
-			kielbasa, pork short loin frankfurter pig boudin biltong t-bone. Boudin rump venison tongue andouille pancetta
-			short loin sirloin alcatra tri-tip pork short ribs capicola. Beef ribs kielbasa boudin corned beef, cupim
-			ground round tri-tip flank tongue. Bacon andouille salami bresaola jowl.</p></div>
+	<body>
+		<header>
+			<a class="logo" href="#">Bacon Ipsum</a>
+
+			<div class="row">
+				<div class="col-md-12">
+					<div id="mouse" onmouseover="mouseOver()" onmouseout="mouseOut()">
+						<p>
+							Bacon ipsum dolor amet filet mignon pancetta corned beef swine drumstick. Alcatra pancetta cow
+							rump, bacon tail
+							biltong pork belly pork sirloin strip steak chuck. Brisket turkey swine spare ribs short ribs
+							leberkas
+							kielbasa, pork short loin frankfurter pig boudin biltong t-bone. Boudin rump venison tongue
+							andouille pancetta
+							short loin sirloin alcatra tri-tip pork short ribs capicola. Beef ribs kielbasa boudin corned beef,
+							cupim
+							ground round tri-tip flank tongue. Bacon andouille salami bresaola jowl.
+						</p>
+					</div>
 					<div class="row">
 						<div class="col-md-12">
-		<p>Strip steak alcatra chicken jerky ham hock turducken meatball spare ribs beef shankle corned beef pancetta
-		salami shoulder. Leberkas tenderloin meatball filet mignon t-bone. Ham sausage chuck, ham hock corned beef shank
-		pancetta meatloaf short loin flank. Chuck tongue boudin, pork belly chicken porchetta hamburger jowl ham hock
-		doner rump salami picanha turkey pig. Swine alcatra rump t-bone pork loin sirloin prosciutto meatloaf bresaola
-		drumstick biltong porchetta. Strip steak prosciutto flank pork pork belly biltong venison hamburger t-bone swine.</p>
+							<div id="text">
+							<p>Strip steak alcatra chicken jerky ham hock turducken meatball spare ribs beef shankle corned
+								beef pancetta
+								salami shoulder. Leberkas tenderloin meatball filet mignon t-bone. Ham sausage chuck, ham hock
+								corned beef shank
+								pancetta meatloaf short loin flank. Chuck tongue boudin, pork belly chicken porchetta hamburger
+								jowl ham hock
+								doner rump salami picanha turkey pig. Swine alcatra rump t-bone pork loin sirloin prosciutto
+								meatloaf bresaola
+								drumstick biltong porchetta. Strip steak prosciutto flank pork pork belly biltong venison
+								hamburger t-bone swine.
+							</p>
+						</div>
+
 							<div class="row">
 								<div class="col-md-12">
-		<p>Pork tri-tip strip steak cupim drumstick. Venison pork belly beef ribs leberkas drumstick. Salami biltong
-			sausage andouille chuck, shank corned beef shoulder sirloin tenderloin pig jerky. Drumstick ham flank
-			tenderloin, pastrami boudin chuck. Bacon beef ribs meatball pork loin andouille pork chop beef ribeye,
-			biltong venison cupim pig.</p>
-						<div id="mouse" onmouseover="mouseOver();" onmouseout="mouseOut();">Hover over me.</div>
-			</div>
-		</body>
+									<p>Pork tri-tip strip steak cupim drumstick. Venison pork belly beef ribs leberkas drumstick.
+										Salami biltong
+										sausage andouille chuck, shank corned beef shoulder sirloin tenderloin pig jerky.
+										Drumstick ham flank
+										tenderloin, pastrami boudin chuck. Bacon beef ribs meatball pork loin andouille pork chop
+										beef ribeye,
+										biltong venison cupim pig.</p>
+								</div>
+							</div>
+	</body>
 </html>
--- javascript/style.js
+++ javascript/style.js
@@ -1,6 +1,15 @@
 /**
- * changes background color when moused out
+ * changes background color when moused over
  **/
 function mouseOver() {
 	document.getElementById("mouse").style.backgroundColor = "blue";
+}
+
+function mouseOut() {
+	document.getElementById("mouse").style.backgroundColor = "red";
+
+}
+
+function changeText(){
+	document.getElmentById("text").style.textScript = "yellow";
 }
