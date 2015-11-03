5b60ace8bf071382dc8b114729d70090294fdcf2
gerald-fongwe <geraldjam2004@yahoo.com>
2015-11-03 11:08:30 -0700

    added becon ipsuml to php

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet"/>
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" rel="stylesheet"/>
+		<script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/js-cookie/2.0.2/js.cookie.min.js"></script>
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.14.0/jquery.validate.min.js"></script>
+		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.14.0/additional-methods.min.js"></script>
+		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+		<title>javascript exercise</title>
+	</head>
+		<body class="sfooter">
+			<div class="sfooter-content">
+	 			<header>
+					<a class="logo" href="#">Bacon Ipsum</a>
+						<div class="row">
+							<div class="col-md-12">
+					<p>Bacon ipsum dolor amet filet mignon pancetta corned beef swine drumstick. Alcatra pancetta cow rump, bacon tail
+			biltong pork belly pork sirloin strip steak chuck. Brisket turkey swine spare ribs short ribs leberkas
+			kielbasa, pork short loin frankfurter pig boudin biltong t-bone. Boudin rump venison tongue andouille pancetta
+			short loin sirloin alcatra tri-tip pork short ribs capicola. Beef ribs kielbasa boudin corned beef, cupim
+			ground round tri-tip flank tongue. Bacon andouille salami bresaola jowl.</p>
+					<div class="row">
+						<div class="col-md-12">
+		<p>Strip steak alcatra chicken jerky ham hock turducken meatball spare ribs beef shankle corned beef pancetta
+		salami shoulder. Leberkas tenderloin meatball filet mignon t-bone. Ham sausage chuck, ham hock corned beef shank
+		pancetta meatloaf short loin flank. Chuck tongue boudin, pork belly chicken porchetta hamburger jowl ham hock
+		doner rump salami picanha turkey pig. Swine alcatra rump t-bone pork loin sirloin prosciutto meatloaf bresaola
+		drumstick biltong porchetta. Strip steak prosciutto flank pork pork belly biltong venison hamburger t-bone swine.</p>
+							<div class="row">
+								<div class="col-md-12">
+		<p>Pork tri-tip strip steak cupim drumstick. Venison pork belly beef ribs leberkas drumstick. Salami biltong
+			sausage andouille chuck, shank corned beef shoulder sirloin tenderloin pig jerky. Drumstick ham flank
+			tenderloin, pastrami boudin chuck. Bacon beef ribs meatball pork loin andouille pork chop beef ribeye,
+			biltong venison cupim pig.</p>
+			</div>
+</html>
